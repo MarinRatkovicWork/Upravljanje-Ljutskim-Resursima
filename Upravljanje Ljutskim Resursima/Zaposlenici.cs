4334917b4819f7783ac9c49f2d155182484f062a
MarinRatkovicWork <86005829+MarinRatkovicWork@users.noreply.github.com>
2021-11-20 08:26:54 +0100

    Uspio složiti da korisnik nemože mjenjati podatke direktno iz kucica sve dok ne klikne na uredi gumb

--- Upravljanje Ljutskim Resursima/Zaposlenici.cs
+++ Upravljanje Ljutskim Resursima/Zaposlenici.cs
@@ -24,12 +24,14 @@ namespace Upravljanje_Ljutskim_Resursima
 
         private void Zaposlenici_Load(object sender, EventArgs e)
         {
+            this.zaposlenikTableTableAdapter.ClearBeforeFill = false;
             this.zaposlenikTableTableAdapter.Fill(this.zAPOSLENICIDBDataSet.ZaposlenikTable);
             Greska.Visible = false;
             DatuRođenjaZaposlenika.CustomFormat = " ";
-            DatuRođenjaZaposlenika.Format = DateTimePickerFormat.Custom;
+            DatuRođenjaZaposlenika.Format = DateTimePickerFormat.Custom;          
         }    
 
+        
         private void button4_Click(object sender, EventArgs e)
         {
             Zaposlenici.ActiveForm.Hide();
@@ -50,8 +52,36 @@ namespace Upravljanje_Ljutskim_Resursima
                 else
                 {
                 this.zaposlenikTableBindingSource.AddNew();
-                this.zaposlenikTableBindingSource.EndEdit();
-                this.zaposlenikTableTableAdapter.Update(this.zAPOSLENICIDBDataSet.ZaposlenikTable);
+                /*
+                ZAPOSLENICIDBDataSet.ZaposlenikTableRow novizaposlenikTableRow;
+                novizaposlenikTableRow = zaposlenicidbDataSet1.ZaposlenikTable.NewZaposlenikTableRow();
+
+                novizaposlenikTableRow.BrojMobZaposlenikaDB = BrojMobitelaZaposlenika.Text;
+                novizaposlenikTableRow.ImeZaposlenikaDB = ImeZaposlenika.Text;
+                novizaposlenikTableRow.PrezimeZaposlenikaDB = PrezimeZaposlenika.Text;
+                novizaposlenikTableRow.SpolZaposlenikaDB = SoplZaposlenika.Text;
+                novizaposlenikTableRow.ObrazovanjeZaposlenikaDB= ObrazovanjeZaposlenika.Text;
+                novizaposlenikTableRow.PozicijaZaposlenikaDB = PozicijaZaposlenika.Text;
+                novizaposlenikTableRow.AdresaZaposlenikaDB = AdresaZaposlenika.Text;
+                novizaposlenikTableRow.GodRodZaposlenikaDB = DatuRođenjaZaposlenika.Value;
+
+                this.zaposlenicidbDataSet1.ZaposlenikTable.Rows.Add(novizaposlenikTableRow);*/
+
+                try
+                {              
+                    this.zaposlenikTableTableAdapter.Insert(brojZaposlenikaDBDataGridViewTextBoxColumn.Index, ImeZaposlenika.Text,
+                        PrezimeZaposlenika.Text, AdresaZaposlenika.Text, BrojMobitelaZaposlenika.Text, SoplZaposlenika.Text, 
+                        PozicijaZaposlenika.Text, ObrazovanjeZaposlenika.Text, DatuRođenjaZaposlenika.Value);
+                    MessageBox.Show("Update successful");
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Update failed");
+                }
+
+                
+                
+               
                
                     Greska.Visible = false;
                     DatuRođenjaZaposlenika.CustomFormat = " ";
@@ -76,19 +106,32 @@ namespace Upravljanje_Ljutskim_Resursima
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Uredi(true);
             this.zaposlenikTableBindingSource.EndEdit();
 
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
 
         private void DatuRođenjaZaposlenika_ValueChanged(object sender, EventArgs e)
         {
             DatuRođenjaZaposlenika.CustomFormat = "dd/MM/yyyy";
             DatuRođenjaZaposlenika.Format = DateTimePickerFormat.Custom;
+        }
+        private void Uredi(bool uredi)
+        {
+            BrojMobitelaZaposlenika.Enabled = uredi;
+            ImeZaposlenika.Enabled = uredi;
+            PrezimeZaposlenika.Enabled = uredi;
+            SoplZaposlenika.Enabled = uredi;
+            ObrazovanjeZaposlenika.Enabled = uredi;
+            PozicijaZaposlenika.Enabled = uredi;
+            AdresaZaposlenika.Enabled = uredi;
+            DatuRođenjaZaposlenika.Enabled = uredi;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Uredi(false);          
         }
     }
 }
